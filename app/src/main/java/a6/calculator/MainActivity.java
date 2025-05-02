4f2d16c1e885b1becc38cb9f6d6a8ad6014dc5bc
Jose Granados Ruiz <151814924+RM64@users.noreply.github.com>
2025-05-01 21:45:11 -0500

    Commit

--- app/src/main/java/a6/calculator/MainActivity.java
+++ app/src/main/java/a6/calculator/MainActivity.java
@@ -1,12 +1,18 @@
 package a6.calculator;
 
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import a6.calculator.model.StackCalculator;
+import a6.calculator.controller.CalculationController;
+import a6.calculator.controller.DisplayStates;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,10 +21,59 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.display), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // Instantiate Display Texts
+        TextView displayView = findViewById(R.id.display);
+        TextView stackView = findViewById(R.id.stack);
+
+        StackCalculator sc = new StackCalculator();
+        DisplayStates ds = null;
+        CalculationController cc = new CalculationController(sc, displayView, stackView, null);
+
+        // Instantiate Buttons
+        Button zeroButton = findViewById(R.id.zero);
+        Button oneButton = findViewById(R.id.one);
+        Button twoButton = findViewById(R.id.two);
+        Button threeButton = findViewById(R.id.three);
+        Button fourButton = findViewById(R.id.four);
+        Button fiveButton = findViewById(R.id.five);
+        Button sixButton = findViewById(R.id.six);
+        Button sevenButton = findViewById(R.id.seven);
+        Button eightButton = findViewById(R.id.eight);
+        Button nineButton = findViewById(R.id.nine);
+        Button plusminusButton = findViewById(R.id.plusminus);
+        Button clearButton = findViewById(R.id.clear);
+        Button enterButton = findViewById(R.id.enter);
+        Button addButton = findViewById(R.id.add);
+        Button subButton = findViewById(R.id.sub);
+        Button mulButton = findViewById(R.id.mul);
+        Button divButton = findViewById(R.id.div);
+
+        // Instantiate Button onClickListeners
+        zeroButton.setOnClickListener(v -> cc.inputDigit(0));
+        oneButton.setOnClickListener(v -> cc.inputDigit(1));
+        twoButton.setOnClickListener(v -> cc.inputDigit(2));
+        threeButton.setOnClickListener(v -> cc.inputDigit(3));
+        fourButton.setOnClickListener(v -> cc.inputDigit(4));
+        fiveButton.setOnClickListener(v -> cc.inputDigit(5));
+        sixButton.setOnClickListener(v -> cc.inputDigit(6));
+        sevenButton.setOnClickListener(v -> cc.inputDigit(7));
+        eightButton.setOnClickListener(v -> cc.inputDigit(8));
+        nineButton.setOnClickListener(v -> cc.inputDigit(9));
+
+        enterButton.setOnClickListener(v -> cc.inputEnter());
+        clearButton.setOnClickListener(v -> cc.inputClear());
+        plusminusButton.setOnClickListener(v -> cc.inputPlusMinus());
+
+
+        addButton.setOnClickListener(v -> cc.performOperation(() -> sc.add()));
+        subButton.setOnClickListener(v -> cc.performOperation(() -> sc.subtract()));
+        mulButton.setOnClickListener(v -> cc.performOperation(() -> sc.multiply()));
+        divButton.setOnClickListener(v -> cc.performOperation(() -> sc.divide()));
     }
 }
--- app/src/main/java/a6/calculator/model/StackCalculator.java
+++ app/src/main/java/a6/calculator/model/StackCalculator.java
@@ -3,5 +3,89 @@ package a6.calculator.model;
 import java.util.LinkedList;
 
 public class StackCalculator extends LinkedList<Integer> {
+    public void add() throws NotEnoughArgumentsException, OverflowException {
+        // Check for NotEnoughArgumentsException
+        if (this.size() < 2) {
+            throw new NotEnoughArgumentsException();
+        }
 
+        // Popping two numbers to add from the stack
+        int sum;
+        int a = this.pop();
+        int b = this.pop();
+
+        // Check for OverflowException after handling addition
+        try {
+            sum = Math.addExact(a, b);
+        } catch (ArithmeticException ae) {
+            throw new OverflowException();
+        }
+
+        this.push(sum);
+    }
+
+    public void subtract() throws NotEnoughArgumentsException, OverflowException  {
+        // Check for NotEnoughArgumentsException
+        if (this.size() < 2) {
+            throw new NotEnoughArgumentsException();
+        }
+
+        int difference;
+        int b = this.pop();
+        int a = this.pop();
+
+        // Check for OverflowException after handling subtraction
+        try {
+            difference = Math.subtractExact(a, b);
+        } catch (ArithmeticException ae) {
+            throw new OverflowException();
+        }
+
+        this.push(difference);
+    }
+
+    public void multiply() throws NotEnoughArgumentsException, OverflowException {
+        // Check for NotEnoughArgumentsException
+        if (this.size() < 2) {
+            throw new NotEnoughArgumentsException();
+        }
+
+        int product;
+        int a = this.pop();
+        int b = this.pop();
+
+        // Check for OverflowException after handling multiplication
+        try {
+            product = Math.multiplyExact(a, b);
+        } catch (ArithmeticException ae) {
+            throw new OverflowException();
+        }
+
+        this.push(product);
+    }
+
+    public void divide() throws NotEnoughArgumentsException, DivisionByZeroException, OverflowException {
+        // Check for NotEnoughArgumentsException
+        if (this.size() < 2) {
+            throw new NotEnoughArgumentsException();
+        }
+
+        // Check for DivisionByZeroException
+        int b = this.pop();
+        if (b == 0) {
+            throw new DivisionByZeroException();
+        }
+
+        int quotient;
+        int a = this.pop();
+
+        // Check for OverflowException after handling division
+        try {
+            quotient = Math.divideExact(a, b);
+        } catch (ArithmeticException ae) {
+            throw new OverflowException();
+        }
+
+        this.push(quotient);
+    }
 }
