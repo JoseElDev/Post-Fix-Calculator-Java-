b326380d0a921e871f0a10f82ffde9a065f7fc01
Javier <62152194+Vettel53@users.noreply.github.com>
2025-05-09 20:57:31 -0500

    javadoc DivisionByZeroException.java

--- app/src/main/java/a6/calculator/model/DivisionByZeroException.java
+++ app/src/main/java/a6/calculator/model/DivisionByZeroException.java
@@ -1,6 +1,12 @@
 package a6.calculator.model;
 
+/**
+ * Custom exception thrown when a division by zero occurs in the calculator.
+ */
 public class DivisionByZeroException extends Exception {
+    /**
+     * Constructs a new custom DivisionByZeroException with a default error message.
+     */
     public DivisionByZeroException() {
         super("Error. Division by zero is not allowed.");
     }
