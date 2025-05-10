a19e0db5358898e0091e931fc3060cf9d2a22f5e
Javier <62152194+Vettel53@users.noreply.github.com>
2025-05-09 21:04:43 -0500

    javadoc NotEnoughArgumentsException.java

--- app/src/main/java/a6/calculator/model/NotEnoughArgumentsException.java
+++ app/src/main/java/a6/calculator/model/NotEnoughArgumentsException.java
@@ -1,6 +1,13 @@
 package a6.calculator.model;
 
+/**
+ * Exception thrown when an operation is attempted with insufficient arguments in the stack.
+ */
 public class NotEnoughArgumentsException extends Exception {
+
+    /**
+     * Constructs a new NotEnoughArgumentsException with a custom error message.
+     */
     public NotEnoughArgumentsException() {
         super("Error. Not enough arguments.");
     }
