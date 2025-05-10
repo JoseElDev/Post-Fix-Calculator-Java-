449b8e6fad0611e60995043dd1df22f37b047d93
Javier <62152194+Vettel53@users.noreply.github.com>
2025-05-09 21:01:50 -0500

    Javadoc CalculatorView.java

--- app/src/main/java/a6/calculator/view/CalculatorView.java
+++ app/src/main/java/a6/calculator/view/CalculatorView.java
@@ -3,31 +3,56 @@ package a6.calculator.view;
 import android.widget.TextView;
 import java.util.List;
 
+/**
+ * Represents the user interface for the calculator that handles display updates.
+ */
 public class CalculatorView {
     private final TextView displayView;
     private final TextView stackView;
 
+    /**
+     * Constructs a CalculatorView instance.
+     *
+     * @param displayView The TextView to display calculator results.
+     * @param stackView   The TextView to display the stack contents.
+     */
     public CalculatorView(TextView displayView, TextView stackView) {
         this.displayView = displayView;
         this.stackView   = stackView;
     }
 
-    // Displays a certain value
+    /**
+     * Displays a result value on the calculator screen.
+     *
+     * @param value The value to display.
+     */
     public void displayResult(String value) {
         displayView.setText(value);
     }
 
-    // Displays the result of an operation between two numbers
+    /**
+     * Displays the user's input with an "_" suffix.
+     *
+     * @param input The input value to display.
+     */
     public void displayInput(String input) {
         displayView.setText(input + "_");
     }
 
-    // Displays the error message
+    /**
+     * Displays an error message on the calculator screen.
+     *
+     * @param text The error message to display.
+     */
     public void displayError(String text) {
         displayView.setText(text);
     }
 
-    // Displays the stack above the input
+    /**
+     * Displays the stack contents above the input field.
+     *
+     * @param elements The list of integers in the calculator stack.
+     */
     public void displayStack(List<Integer> elements) {
         StringBuilder sb = new StringBuilder();
         for (Integer v : elements) {
