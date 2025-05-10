b54033cf5d453e393bc724f512f3e981a8d6e643
Javier <62152194+Vettel53@users.noreply.github.com>
2025-05-09 20:58:07 -0500

    Javadoc OverflowException.java

--- app/src/main/java/a6/calculator/model/OverflowException.java
+++ app/src/main/java/a6/calculator/model/OverflowException.java
@@ -1,6 +1,12 @@
 package a6.calculator.model;
 
+/**
+ * Custom Exception thrown when an overflow occurs in the calculator.
+ */
 public class OverflowException extends Exception {
+    /**
+     * Constructs a new custom OverflowException with a default error message.
+     */
     public OverflowException() {
         super("Error. An overflow occurred.");
     }
