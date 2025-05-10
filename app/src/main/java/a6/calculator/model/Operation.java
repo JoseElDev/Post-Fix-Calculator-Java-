ab75a6e2e9d57dee84b971c7df3bc342d58b1988
Javier <62152194+Vettel53@users.noreply.github.com>
2025-05-09 21:04:10 -0500

    javadoc Operation.java

--- app/src/main/java/a6/calculator/model/Operation.java
+++ app/src/main/java/a6/calculator/model/Operation.java
@@ -1,6 +1,17 @@
 package a6.calculator.model;
 
+/**
+ * Represents an arithmetic operation that can be applied to a stack-based calculator.
+ */
 public interface Operation {
+
+    /**
+     * Applies the operation on the calculator stack.
+     *
+     * @throws NotEnoughArgumentsException If there are insufficient arguments for the operation.
+     * @throws OverflowException If the result of the operation causes an overflow.
+     * @throws DivisionByZeroException If the operation attempts to divide by zero.
+     */
     void apply() throws NotEnoughArgumentsException,
                         OverflowException,
                         DivisionByZeroException;
